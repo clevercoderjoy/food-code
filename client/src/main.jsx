1466e8a439fd990ca54f98ba58de1b6fa0e996e7
clevercoderjoy <clevercoderjoy@gmail.com>
2024-10-24 20:42:42 +0530

    toast fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,21 @@ import { router } from './routes/Routes.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
-      <ToastContainer />
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        pauseOnFocusLoss
+        style={{ margin: 0, padding: 0, bottom: "-15px", left: "2px" }}
+      />
+
     </Provider>
   </React.StrictMode>
 )
